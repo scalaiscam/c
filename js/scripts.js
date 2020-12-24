183a358bafada682408bba282e3d1466e61deb11
Camille Scalais <camille_scalais@hotmail.com>
2020-12-25 00:31:46 +0100

    avancement jeudi

---
+++ js/scripts.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){
+
+$("a").click(function(e){
+    e.preventDefault();
+
+    var valeur = $(this).attr("data-attribute");
+    console.log(valeur);
+
+    $(".content:not("+valeur+")").fadeOut("fast");
+    $(valeur).show();
+
+}); //click
+
+
+}); //ready
