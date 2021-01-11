7c967781b4a82d18f98e1019cde625f3ab0aa82a
Camille Scalais <camille_scalais@hotmail.com>
2021-01-11 03:39:34 +0100

    sauf contact

--- js/scripts.js
+++ js/scripts.js
@@ -15,19 +15,99 @@
 //   }); //ready
 
 
-// ------------------------- function read more read less -------------------------
-// function myFunction() {
-//     var dots = document.getElementById("dots");
-//     var moreText = document.getElementById("more");
-//     var btnText = document.getElementById("myBtn");
-  
-//     if (dots.style.display === "none") {
-//       dots.style.display = "inline";
-//       btnText.innerHTML = "Read more";
-//       moreText.style.display = "none";
-//     } else {
-//       dots.style.display = "none";
-//       btnText.innerHTML = "Read less";
-//       moreText.style.display = "inline";
-//     }
-//   }
+
+//--------------------------------------------------------cookie---------------------------------------------------------------------
+
+var cookie = Cookies.get('cook');
+
+if(cookie === undefined) {
+    console.log('empty');
+} else {
+    console.log('exist');
+    $('#bar-cookie').remove();
+}
+
+$('#cookie-ok').click(function(e){
+    e.preventDefault();
+    $('#bar-cookie').fadeOut();
+    Cookies.set('cook', 'valeur du cookie', { expires: 365});
+});
+
+//--------------------------------------------------------ssmooth scroll top---------------------------------------------------------------------
+
+var btn = $('#buttontop');
+
+$(window).scroll(function() {
+  if ($(window).scrollTop() > 300) {
+    btn.addClass('show');
+  } else {
+    btn.removeClass('show');
+  }
+});
+
+btn.on('click', function(e) {
+  e.preventDefault();
+  $('html, body').animate({scrollTop:0}, '300');
+});
+
+//--------------------------------------------------------formulaire ajax---------------------------------------------------------------------
+
+$('#valide-form').click(function(e){
+    e.preventDefault()
+    var mail1 = $("#insert-mail-1").val();
+    var mail2 = $("#insert-mail-2").val();
+    var checkbox = $('#check-actu').is(':checked');
+    var nom = $("#insert-name").val();
+    var prenom = $("#insert-surname").val();
+
+    console.log(checkbox)
+    console.log(mail1)
+    console.log(mail2)
+    console.log(prenom)
+    console.log(nom)
+
+    if (checkbox == true){
+        checkboxtrue = " Vous serez tenu au courant des news Dance Brussels !"
+    }else {
+        checkboxtrue = " "
+    }
+    
+    $.ajax({
+        url : 'form.php',
+        type : 'POST',
+        data : {
+            mailunenphp : mail1,
+            maildeuxenphp : mail2,
+            checkboxenphp : checkboxtrue,
+            nomenphp : nom,
+            prenomenphp : prenom
+        },
+        dataType : 'html',
+        success : function(data){
+            $('#reponse-form').html(data);
+        }
+    })
+}) //fin click
+
+
+$('#envoyer-form').click(function(e){
+    e.preventDefault()
+})
+
+//--------------------------------------------------------header fixe---------------------------------------------------------------------
+
+
+
+//## Demo
+//https://bcjdevelopment.github.io/butter.js/
+// smooth like buttah...
+
+// cancel momentum scrolling
+
+  // set options when initializing butter.js
+  var options = {
+    wrapperId: 'customDefaultId',
+    wrapperDamper: 0.10,
+    cancelOnTouch: true
+  };
+ 
